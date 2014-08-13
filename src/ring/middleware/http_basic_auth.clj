52344a5b95ef6ce37ac9351ecd02fb703148f844
Adrian Smith <smith.adriane@gmail.com>
2014-08-13 16:56:53 -0700

    divided auth wrappers into multiple subwrappers.

--- src/ring/middleware/http_basic_auth.clj
+++ src/ring/middleware/http_basic_auth.clj
@@ -2,7 +2,6 @@
   "Ring middleware for basic HTTP authentication."
   (:use [remvee.base64 :as base64]))
 
-(declare *user*)
 
 (defn- get-credentials [req]
   (let [auth ((req :headers) "authorization")
@@ -11,41 +10,39 @@
         password (and cred (last (re-find #":(.*)$" cred)))]
     [username password]))
 
-(defn wrap-with-auth
-  "Wrap response with a basic authentication challenge as described in
-  RFC2617 section 2.
+(let [out *out*]
+  (defn p [& args]
+    (binding [*out* out]
+      (apply println args))))
 
-  The authenticate function is called with two parameters, the
-  username and password, and should return a value when the login is
-  valid."
-
-  [app authenticate]
+(defn wrap-with-credentials [app]
   (fn [req]
     (let [[username password] (get-credentials req)]
-      (binding [*user* (authenticate username password)]
-        (app req)))))
+      (app (-> req
+               (assoc-in [:auth :username] username)
+               (assoc-in [:auth :password] password))))))
 
-(defn wrap-require-auth
-  "The realm is a descriptive string visible to the visitor.  It,
-  together with the canonical root URL, defines the protected resource
-  on the server.
-
-  The denied-response is a ring response structure which will be
-  returned when authorization fails.  The appropriate status and
-  authentication headers will be merged into it.  It defaults to plain
-  text 'access denied' response."
-
-  [app authenticate & [realm denied-response]]
+(defn wrap-with-authenticated? [app authenticated?]
   (fn [req]
-    (let [[username password] (get-credentials req)]
-      (binding [*user* (authenticate username password)]
-        (if *user*
-          (app req)
-          (assoc
-            (merge {:headers {"Content-Type" "text/plain"}
-                    :body "HTTP authentication required."}
-              denied-response)
-            :status  401
-            :headers (merge (:headers denied-response)
-              {"WWW-Authenticate" (format
-                "Basic realm=\"%s\"" (or realm "Restricted Area"))})))))))
+    (let [{{:keys [username password]} :auth} req]
+      (app (assoc-in req [:auth :authenticated?] (boolean (authenticated? req username password)))))))
+
+(defn wrap-with-auth-headers [app & [realm denied-response]]
+  (fn [req]
+    (if (get-in req [:auth :authenticated?])
+      (app req)
+      (assoc
+          (merge {:headers {"Content-Type" "text/plain"}
+                  :body "HTTP authentication required."}
+                 denied-response)
+        :status  401
+        :headers (merge (:headers denied-response)
+                        {"WWW-Authenticate" (format
+                                             "Basic realm=\"%s\"" (or realm "Restricted Area"))})))))
+
+(defn wrap-auth [handler authenticated? & [realm denied-response]]
+  (-> handler
+      (wrap-with-auth-headers realm denied-response)
+      (wrap-with-authenticated? authenticated?)
+      (wrap-with-credentials)))
+
