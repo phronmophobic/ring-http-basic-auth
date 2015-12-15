838df939ecbbe58ef9451961975ac9c17667a932
Adrian Smith <smith.adriane@gmail.com>
2015-12-15 13:47:57 -0800

    Revert "divided auth wrappers into multiple subwrappers."
    This reverts commit 52344a5b95ef6ce37ac9351ecd02fb703148f844.

--- project.clj
+++ project.clj
@@ -1,8 +1,5 @@
-(defproject phronmophobic/ring-http-basic-auth "1.0.2"
+(defproject ring-http-basic-auth "0.0.1"
   :description "Ring middleware for basic HTTP authentication."
-  :url "https://github.com/phronmophobic/ring-http-basic-auth"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
                  [clj-base64 "0.0.1"]])
--- src/ring/middleware/http_basic_auth.clj
+++ src/ring/middleware/http_basic_auth.clj
@@ -2,6 +2,7 @@
   "Ring middleware for basic HTTP authentication."
   (:use [remvee.base64 :as base64]))
 
+(declare *user*)
 
 (defn- get-credentials [req]
   (let [auth ((req :headers) "authorization")
@@ -10,39 +11,41 @@
         password (and cred (last (re-find #":(.*)$" cred)))]
     [username password]))
 
-(let [out *out*]
-  (defn p [& args]
-    (binding [*out* out]
-      (apply println args))))
+(defn wrap-with-auth
+  "Wrap response with a basic authentication challenge as described in
+  RFC2617 section 2.
 
-(defn wrap-with-credentials [app]
+  The authenticate function is called with two parameters, the
+  username and password, and should return a value when the login is
+  valid."
+
+  [app authenticate]
   (fn [req]
     (let [[username password] (get-credentials req)]
-      (app (-> req
-               (assoc-in [:auth :username] username)
-               (assoc-in [:auth :password] password))))))
+      (binding [*user* (authenticate username password)]
+        (app req)))))
 
-(defn wrap-with-authenticated? [app authenticated?]
+(defn wrap-require-auth
+  "The realm is a descriptive string visible to the visitor.  It,
+  together with the canonical root URL, defines the protected resource
+  on the server.
+
+  The denied-response is a ring response structure which will be
+  returned when authorization fails.  The appropriate status and
+  authentication headers will be merged into it.  It defaults to plain
+  text 'access denied' response."
+
+  [app authenticate & [realm denied-response]]
   (fn [req]
-    (let [{{:keys [username password]} :auth} req]
-      (app (assoc-in req [:auth :authenticated?] (boolean (authenticated? req username password)))))))
-
-(defn wrap-with-auth-headers [app & [realm denied-response]]
-  (fn [req]
-    (if (get-in req [:auth :authenticated?])
-      (app req)
-      (assoc
-          (merge {:headers {"Content-Type" "text/plain"}
-                  :body "HTTP authentication required."}
-                 denied-response)
-        :status  401
-        :headers (merge (:headers denied-response)
-                        {"WWW-Authenticate" (format
-                                             "Basic realm=\"%s\"" (or realm "Restricted Area"))})))))
-
-(defn wrap-auth [handler authenticated? & [realm denied-response]]
-  (-> handler
-      (wrap-with-auth-headers realm denied-response)
-      (wrap-with-authenticated? authenticated?)
-      (wrap-with-credentials)))
-
+    (let [[username password] (get-credentials req)]
+      (binding [*user* (authenticate username password)]
+        (if *user*
+          (app req)
+          (assoc
+            (merge {:headers {"Content-Type" "text/plain"}
+                    :body "HTTP authentication required."}
+              denied-response)
+            :status  401
+            :headers (merge (:headers denied-response)
+              {"WWW-Authenticate" (format
+                "Basic realm=\"%s\"" (or realm "Restricted Area"))})))))))
